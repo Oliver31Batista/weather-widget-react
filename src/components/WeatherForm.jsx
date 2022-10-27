a74a4b9efe6a6a928e048e1dee987a25d2c7c851
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-10-26 22:58:38 -0400

    final design (for now)

--- src/components/WeatherForm.jsx
+++ src/components/WeatherForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./weatherForm.module.css";
+import loupe from "../imgs/loupe.png"
 
 export const WeatherForm = ({ onChangeCity }) => {
   const [city, setCity] = useState("");
@@ -17,7 +18,8 @@ export const WeatherForm = ({ onChangeCity }) => {
 
   return (
     <form onSubmit={handleSubmit} className={styles.container}>
-      <input type="text" onChange={onChange} className={styles.input} />
+      <input type="text" onChange={onChange} className={styles.searchTerm} placeholder="Search location..."/>
+      <button className={styles.searchButton}><img src={loupe} alt="search icon"/></button>
     </form>
   );
 };
--- src/components/WeatherMainInfo.jsx
+++ src/components/WeatherMainInfo.jsx
@@ -39,7 +39,7 @@ export const WeatherMainInfo = ({ weather }) => {
         title="map"
         src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d20123.265672124126!2d${weather?.location.lon}1!3d${weather?.location.lat}4!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sdo!4v1666709574587!5m2!1ses-419!2sdo`}
         width="100%"
-        height="390"
+        height="300"
         style={{ border: 0 }}
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
